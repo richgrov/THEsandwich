07a746276b5c42d718522b91f87b140c43c8879a
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-16 02:56:05 -0600

    Add gene_e bot

---
+++ gene_e/__main__.py
@@ -0,0 +1,26 @@
+import discord
+import json
+
+with open("config.json", "r") as file:
+    config = json.load(file)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print("Online")
+
+
+@client.event
+async def on_message(msg: discord.Message):
+    if msg.author == client.user:
+        return
+
+    print(msg.content)
+
+
+client.run(config["token"])
