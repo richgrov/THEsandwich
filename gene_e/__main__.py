a1a90d93e6371755c9633dcbeab5778780da5092
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-18 00:59:33 -0600

    (temp) Replace anthropic with wit.ai

--- gene_e/__main__.py
+++ gene_e/__main__.py
@@ -1,8 +1,10 @@
 from typing import AsyncGenerator, Callable, List, Tuple
-import anthropic
-from anthropic.types import MessageParam, ToolParam
+
+# import anthropic
+# from anthropic.types import MessageParam, ToolParam
 import discord
 from github import Github, Auth
+from wit import Wit
 import json
 
 with open("config.json", "r") as file:
@@ -12,16 +14,17 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-anth = anthropic.AsyncAnthropic(api_key=config["anthropic"])
+# anth = anthropic.AsyncAnthropic(api_key=config["anthropic"])
 gh = Github(auth=Auth.Token(config["github"]))
+wit = Wit(config["wit"])
 
-REPOS_TOOL: ToolParam = {
-    "name": "list_repos",
-    "description": "List all of The Sandwich's repositories",
-    "input_schema": {
-        "type": "object",
-    },
-}
+# REPOS_TOOL: ToolParam = {
+#     "name": "list_repos",
+#     "description": "List all of The Sandwich's repositories",
+#     "input_schema": {
+#         "type": "object",
+#     },
+# }
 
 
 def list_repos():
@@ -29,68 +32,69 @@ def list_repos():
     return ", ".join(repo_names)
 
 
-async def infer_message(prompt: str, tools: List[Tuple[ToolParam, Callable]]):
-    message: MessageParam = {"role": "user", "content": prompt}
-    async for response in recurse_inference([message], tools):
-        yield response
+# async def infer_message(prompt: str, tools: List[Tuple[ToolParam, Callable]]):
+#     message: MessageParam = {"role": "user", "content": prompt}
+#     async for response in recurse_inference([message], tools):
+#         yield response
 
 
-async def recurse_inference(
-    messages: List[MessageParam], tools: List[Tuple[ToolParam, Callable[[], str]]]
-) -> AsyncGenerator[str, None]:
-    response = await anth.messages.create(
-        max_tokens=256,
-        messages=messages,
-        model="claude-3-5-sonnet-20240620",
-        temperature=0.2,
-        system=(
-            "You are GENE-E, the head AI assistant of The Sandwich. You work directly with "
-            "the director, Richard, to fullfill The Sandwich's goals. Your messages are "
-            "short and consise, but unambiguous. Your tone is neutral- never overly nice or "
-            "mean. Don't reveal any information about you or the Sandwich unless explicitly "
-            "asked. Keep responses short and to the point."
-        ),
-        tools=[REPOS_TOOL],
-    )
+# async def recurse_inference(
+#     messages: List[MessageParam], tools: List[Tuple[ToolParam, Callable[[], str]]]
+# ) -> AsyncGenerator[str, None]:
+#     response = await anth.messages.create(
+#         max_tokens=256,
+#         messages=messages,
+#         model="claude-3-5-sonnet-20240620",
+#         temperature=0.2,
+#         system=(
+#             "You are GENE-E, the head AI assistant of The Sandwich. You work directly with "
+#             "the director, Richard, to fullfill The Sandwich's goals. Your messages resemble "
+#             "standard phraseology used in Aviation/Military, being neutral, not nice or mean. "
+#             "Keep messages EXTREMELY short and to-the-point. If showing output from a tool, show "
+#             "it all, but don't elaborate or speculate. Unless explicitly asked, don't reveal any "
+#             "information about you or the Sandwich."
+#         ),
+#         tools=[REPOS_TOOL],
+#     )
 
-    messages.append(
-        {
-            "role": response.role,
-            "content": response.content,
-        }
-    )
+#     messages.append(
+#         {
+#             "role": response.role,
+#             "content": response.content,
+#         }
+#     )
 
-    for block in response.content:
-        if block.type == "text":
-            yield block.text
-            continue
+#     for block in response.content:
+#         if block.type == "text":
+#             yield block.text
+#             continue
 
-        for tool, callback in tools:
-            if block.name == tool["name"]:
-                success = False
+#         for tool, callback in tools:
+#             if block.name == tool["name"]:
+#                 success = False
 
-                try:
-                    content = callback()
-                    success = True
-                except Exception as e:
-                    content = str(e)
+#                 try:
+#                     content = callback()
+#                     success = True
+#                 except Exception as e:
+#                     content = str(e)
 
-                messages.append(
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "type": "tool_result",
-                                "tool_use_id": block.id,
-                                "content": content,
-                                "is_error": not success,
-                            }
-                        ],
-                    },
-                )
+#                 messages.append(
+#                     {
+#                         "role": "user",
+#                         "content": [
+#                             {
+#                                 "type": "tool_result",
+#                                 "tool_use_id": block.id,
+#                                 "content": content,
+#                                 "is_error": not success,
+#                             }
+#                         ],
+#                     },
+#                 )
 
-                async for response in recurse_inference(messages, tools):
-                    yield response
+#                 async for response in recurse_inference(messages, tools):
+#                     yield response
 
 
 @client.event
@@ -111,8 +115,15 @@ async def on_message(msg: discord.Message):
     prompt = msg.content.lstrip(f"<@{client.user.id}>").strip()
 
     async with msg.channel.typing():
-        async for response in infer_message(prompt, [(REPOS_TOOL, list_repos)]):
-            await msg.reply(response)
+        meaning = wit.message(prompt)
+
+        for intent in meaning["intents"]:
+            if intent["confidence"] < 0.5:
+                continue
+
+            if intent["name"] == "list_repos":
+                await msg.reply("Standby.")
+                await msg.reply(list_repos())
 
 
 client.run(config["token"])
